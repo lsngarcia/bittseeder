06be46b72859a72e1af67d80aec3164724c203fd
Power2All <info@power2all.com>
2026-03-02 15:57:44 +0100

    Initial project, moved from Torrust-Actix

---
+++ src/web/structs/app_state.rs
@@ -0,0 +1,27 @@
+use crate::config::structs::torrents_file::TorrentsFile;
+use crate::stats::shared_stats::SharedStats;
+use std::collections::{
+    HashMap,
+    VecDeque
+};
+use std::path::PathBuf;
+use std::sync::Arc;
+use tokio::sync::{
+    broadcast,
+    watch,
+    Mutex,
+    RwLock,
+};
+
+pub type SessionStore = Arc<Mutex<HashMap<String, std::time::Instant>>>;
+
+pub struct AppState {
+    pub yaml_path: PathBuf,
+    pub shared_file: Arc<RwLock<TorrentsFile>>,
+    pub stats: SharedStats,
+    pub reload_tx: watch::Sender<()>,
+    pub web_password: Option<String>,
+    pub sessions: SessionStore,
+    pub log_tx: broadcast::Sender<String>,
+    pub log_buffer: Arc<std::sync::Mutex<VecDeque<String>>>,
+}
